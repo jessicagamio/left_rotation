ed61e1994e148ccbe8a9bff4aade3e397ee513d4
jessicagamio <jessica.gamio@gmail.com>
2019-12-12 15:24:53 -0800

    corrected join method by converting numbers into strings

--- leftrot.py
+++ leftrot.py
@@ -3,16 +3,18 @@
 def rotate(array,d):
     """
     >>> rotate([1,2,3,4,5], 4)
-    [5,1,2,3,4]
+    '5 1 2 3 4'
     """
 
     i = 0
 
-    while i <= d
-        array = array[1:]+array[0]
+    while i < d:
+        array = array[1:]+ [array[0]]
         i+= 1
 
-    return array.join(" ")
+        string_array=[str(i) for i in array]
+
+    return " ".join(string_array)
 
 
 if __name__=="__main__":
